649d21b27f697108a6da3a26ba3357563ef5156a
VishrutMargam <vishrutm@axioned.com>
2021-11-22 15:03:17 +0530

    added recommended animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,13 +24,20 @@ function showTab(n) {
 }
 
 accordions.forEach((acc, index) => {
-    acc.addEventListener('click', e => showAcc(index+1));
+    acc.addEventListener('click', e => showAcc(acc, index+1));
 })
 
-function showAcc(n) {
-    if(accordions[n-1].classList.contains('show')) {
-        accordions[n-1].classList.remove('show');
+function showAcc(acc, n) {
+
+    var panel = acc.querySelector('div');
+
+    if(panel.style.height) {
+        acc.querySelector('h2').classList.remove('show')
+        panel.style.height = null;
     } else {
-        accordions[n-1].classList.add('show');
+        accordions.forEach(item => item.querySelector('h2').classList.remove('show'))
+        accordions.forEach(item => item.querySelector('div').style.height = null)
+        acc.querySelector('h2').classList.add('show')
+        panel.style.height = panel.scrollHeight + 'px';
     }
 }
