e9a9800cbaabc86012ddb30cbabf4190f5ee7f9d
VishrutMargam <vishrutm@axioned.com>
2021-11-22 13:30:29 +0530

    added accordion functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var tabContent = document.querySelectorAll('.tab-content li');
 var tabs = document.querySelectorAll('.tabs li a');
+var accordions = document.querySelectorAll('.accordion li');
 
 var tabIndex = 1;
+showTab(tabIndex)
 
 tabs.forEach((tab, index) => {
     tab.addEventListener('click', e => changeTab(e, index+1))
@@ -12,10 +14,23 @@ function changeTab(e, n) {
     showTab(tabIndex = n)
 }
 
-function showTab() {
+function showTab(n) {
+
     tabContent.forEach(tab => tab.classList.remove('active'))
     tabs.forEach(tab => tab.classList.remove('active'))
 
     tabContent[tabIndex-1].classList.add('active');
     tabs[tabIndex-1].classList.add('active');
+}
+
+accordions.forEach((acc, index) => {
+    acc.addEventListener('click', e => showAcc(index+1));
+})
+
+function showAcc(n) {
+    if(accordions[n-1].classList.contains('show')) {
+        accordions[n-1].classList.remove('show');
+    } else {
+        accordions[n-1].classList.add('show');
+    }
 }
